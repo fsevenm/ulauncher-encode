d11e00d81d4c4298537f8a939f1e38dedf304d7c
Bruno Paz <brunopaz@sapo.pt>
2018-06-02 12:18:54 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# Ulauncher encode
+
+> [ulauncher](https://ulauncher.io/) Extension for encoding a string into multiple formats (base64, url, html)
+
+## Usage
+
+![demo](demo.gif)
+
+
+## Requirements
+
+* [ulauncher](https://ulauncher.io/)
+* Python >= 2.7
+
+## Install
+
+Open ulauncher preferences window -> extensions -> add extension and paste the following url:
+
+```https://github.com/brpaz/ulauncher-encode```
+ 
+
+## Development
+
+```
+git clone https://github.com/brpaz/ulauncher-encode
+cd ~/.cache/ulauncher_cache/extensions/ulauncher-encode
+ln -s <repo_location> ulauncher-encode
+```
+
+To see your changes, stop ulauncher and run it from the command line with: ```ulauncher -v```.
+
+## License 
+
+MIT
---
+++ main.py
@@ -0,0 +1,57 @@
+import logging
+import base64
+import urllib
+import cgi
+from ulauncher.api.client.Extension import Extension
+from ulauncher.api.client.EventListener import EventListener
+from ulauncher.api.shared.event import KeywordQueryEvent, ItemEnterEvent
+from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
+from ulauncher.api.shared.action.RenderResultListAction import RenderResultListAction
+from ulauncher.api.shared.action.CopyToClipboardAction import CopyToClipboardAction
+from ulauncher.api.shared.action.ExtensionCustomAction import ExtensionCustomAction
+from ulauncher.api.shared.action.HideWindowAction import HideWindowAction
+
+logger = logging.getLogger(__name__)
+
+class EncodeExtension(Extension):
+
+    def __init__(self):
+        logger.info('init Encoding Extension')
+        super(EncodeExtension, self).__init__()
+        self.subscribe(KeywordQueryEvent, KeywordQueryEventListener())
+
+class KeywordQueryEventListener(EventListener):
+
+    def on_event(self, event, extension):
+        items = []
+
+        base64Text = base64.b64encode(event.get_argument())
+        urlEncoded = urllib.quote_plus(event.get_argument())
+
+        htmlEncoded = cgi.escape(event.get_argument())
+        items.append(ExtensionResultItem(icon='images/icon.png',
+                                         name=base64Text,
+                                         description='Base64 Encoded',
+                                         highlightable=False,
+                                         on_enter=CopyToClipboardAction(base64Text)
+                                         ))
+
+        items.append(ExtensionResultItem(icon='images/icon.png',
+                                         name=urlEncoded,
+                                         description='URL Encoded',
+                                         highlightable=False,
+                                         on_enter=CopyToClipboardAction(urlEncoded)
+                                        ))
+
+        items.append(ExtensionResultItem(icon='images/icon.png',
+                                         name=htmlEncoded,
+                                           description='HTML Encoded',
+                                           highlightable=False,
+                                           on_enter=CopyToClipboardAction(
+                                               htmlEncoded)
+                                           ))
+
+        return RenderResultListAction(items)
+
+if __name__ == '__main__':
+    EncodeExtension().run()
