a00450541eea854d0989fcd32021df2cc456a835
fsevenm <ayubasw@gmail.com>
2019-09-24 11:11:36 +0800

    Try to fix 3

--- main.py
+++ main.py
@@ -26,15 +26,20 @@ class KeywordQueryEventListener(EventListener):
         items = []
 
         raw_char = event.get_argument()
-        base64Text = base64.b64encode(raw_char.encode("utf-8"))
-        urlEncoded = urllib.parse.quote_plus(event.get_argument())
+        if raw_char is not None:
+            base64Text = base64.b64encode(raw_char.encode("utf-8"))
+            urlEncoded = urllib.parse.quote_plus(event.get_argument())
 
-        htmlEncoded = cgi.escape(event.get_argument())
+            htmlEncoded = cgi.escape(event.get_argument())
+        else:
+            base64Text = ""
+            urlEncoded = ""
+            htmlEncoded = ""
         items.append(ExtensionResultItem(icon='images/icon.png',
                                          name=base64Text,
                                          description='Base64 Encoded',
                                          highlightable=False,
-                                         on_enter=CopyToClipboardAction(base64Text.decode("utf-8"))
+                                         on_enter=CopyToClipboardAction(base64Text)
                                          ))
 
         items.append(ExtensionResultItem(icon='images/icon.png',
